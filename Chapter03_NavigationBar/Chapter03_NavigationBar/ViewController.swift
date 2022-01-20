a8a35e4971166d951fea342d83418bd42d131cba
한경동 <hkd0694@gmail.com>
2022-01-20 13:28:27 +0900

    Navigation 커스터마이징 Sample 완료

--- Chapter03_NavigationBar/Chapter03_NavigationBar/ViewController.swift
+++ Chapter03_NavigationBar/Chapter03_NavigationBar/ViewController.swift
@@ -34,6 +34,53 @@ class ViewController: UIViewController {
         
         self.navigationItem.titleView = tf
         
+        // 왼쪽 아이템 영역에 들어갈 뷰
+        let back = UIImage(named: "arrow-back")
+        let leftItem = UIBarButtonItem(image: back, style: .plain, target: self, action: nil)
+        
+        // 왼쪽 아이템 영역에 이미지 뷰 설정
+        self.navigationItem.leftBarButtonItem = leftItem
+        
+        // 오른쪽 아이템 영역에 들어갈 컨테이너 뷰
+        let rv = UIView()
+        rv.frame = CGRect(x: 0, y: 0, width: 70, height: 37)
+        
+        let rItem = UIBarButtonItem(customView: rv)
+        self.navigationItem.rightBarButtonItem = rItem
+        
+        // 카운트 값을 표시할 레이블 구성
+        let cnt = UILabel()
+        cnt.frame = CGRect(x: 10, y: 8, width: 20, height: 20)
+        cnt.font = UIFont.boldSystemFont(ofSize: 10)
+        cnt.textColor = UIColor(red: 0.60, green: 0.60, blue: 0.60, alpha: 1.0)
+        cnt.text = "12"
+        cnt.textAlignment = .center
+        
+        // 외곽선
+        cnt.layer.cornerRadius = 3
+        cnt.layer.borderWidth = 2
+        cnt.layer.borderColor = UIColor(red: 0.60, green: 0.60, blue: 0.60, alpha: 1.0).cgColor
+        
+        rv.addSubview(cnt)
+        
+        let more = UIButton(type: .system)
+        more.frame = CGRect(x: 50, y: 10, width: 16, height: 16)
+        more.setImage(UIImage(named: "more"), for: .normal)
+        
+        rv.addSubview(more)
+        
+        // let v = UIView()
+        // v.frame = CGRect(x: 0, y: 0, width: 150, height: 37)
+        // v.backgroundColor = .brown
+        // let leftItem = UIBarButtonItem(customView: v)
+        // self.navigationItem.leftBarButtonItem = leftItem
+        // let rv = UIView()
+        // rv.frame = CGRect(x: 0, y: 0, width: 100, height: 37)
+        // rv.backgroundColor = .red
+        // let rightItem = UIBarButtonItem(customView: rv)
+        // self.navigationItem.rightBarButtonItem = rightItem
+
+        
     }
     
     func initTitleImage() {
